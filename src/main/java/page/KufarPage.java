bd5d0cc88588b8d45061f9ee4879c768a23620ba
Ekaterina Alay <catherine.alay@gmail.com>
2021-09-23 13:23:09 +0300

    fix: fixed problem with changing class names while parsing kufar page. А page is being parsed using tag names instead now

--- src/main/java/page/KufarPage.java
+++ src/main/java/page/KufarPage.java
@@ -19,16 +19,17 @@ public class KufarPage extends AuctionPage {
         try {
             Document doc = Jsoup.connect(url).get();
 
-            Elements cards = doc.getElementsByClass("kf-jklc-a67b1");
+            Element article = doc.getElementsByTag("article").get(0);
+            Elements cards = article.getElementsByTag("a");//doc.getElementsByClass("kf-GvOm-7afe3");
             for (Element card : cards) {
                 try {
                     String itemUrl = card.attr("href");
                     String id = itemUrl.substring(itemUrl.length() - 9);
 
-                    Element imgElement = card.getElementsByClass("kf-jklX-59ddc").first();
+                    Element imgElement = card.getElementsByTag("img").first();
 
-                    String photoUrl = imgElement.attr("data-src");
-                    String caption = imgElement.attr("alt");
+                    String photoUrl = imgElement == null ? "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" : imgElement.attr("data-src");
+                    String caption = imgElement == null ? "Елочная игрушка": imgElement.attr("alt");
 
                     items.add(new ItemDescription(id, itemUrl, photoUrl, caption));
                 } catch (Exception e) {
